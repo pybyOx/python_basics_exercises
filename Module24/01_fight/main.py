096ea66d2f742f936c64bc23ee7801998dedd48f
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:48 +0700

    task 01_fight fixed

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,6 +1,18 @@
 import random
 
 
+def fight(unit, other_unit):
+    while unit.is_alive() and other_unit.is_alive():
+        attacker = random.choice([unit, other_unit])
+        defender = unit if attacker == other_unit else other_unit
+        attacker.hit(defender)
+
+    if unit.health == 0:
+        print('Игрок {} одержал победу!'.format(other_unit.name))
+    else:
+        print('Игрок {} одержал победу!'.format(unit.name))
+
+
 class Unit:
 
     def __init__(self, name):
@@ -10,25 +22,16 @@ class Unit:
     def is_alive(self):
         return self.health > 0
 
+    def defense(self):
+        self.health -= 20
+
     def hit(self, other_unit):
-        other_unit.health -= 20
+        other_unit.defense()
         print('\n{} атакует! У {} осталось здоровья: {}'.format(
             self.name, other_unit.name, other_unit.health))
-
-    def fight(self, other_unit):
-
-        while self.is_alive() and other_unit.is_alive():
-            attacker = random.choice([self, other_unit])
-            defender = self if attacker == other_unit else other_unit
-            attacker.hit(defender)
-
-        if self.health == 0:
-            print('Игрок {} одержал победу!'.format(other_unit.name))
-        else:
-            print('Игрок {} одержал победу!'.format(self.name))
 
 
 unit_1 = Unit('Воин 1')
 unit_2 = Unit('Воин 2')
 print('Начинаем бой!')
-unit_1.fight(unit_2)
+fight(unit_1, unit_2)
