8e5094017f343e912aa7096d5df44adcd7465123
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:41 +0700

    task 04_party solved

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,31 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+while len(guests) > 0:
+
+    print('\nСейчас на вечеринке', len(guests), 'человек:', guests)
+
+    print('Гость пришёл или ушёл?', end=' ')
+    came_or_went = input()
+
+    if came_or_went == 'пришёл' or came_or_went == 'пришел':
+        print('Имя гостя:', end=' ')
+        name_guest = input()
+
+        if len(guests) < 6:
+            print('Привет,', name_guest)
+            guests.append(name_guest)
+        else:
+            print('Прости,', name_guest, 'но мест нет')
+
+    elif came_or_went == 'ушёл' or came_or_went == 'ушел':
+
+        print('Имя гостя:', end=' ')
+        name_guest = input()
+
+        print('Пока,', name_guest, '!')
+        guests.remove(name_guest)
+
+    else:
+        print('Ошибка ввода')
+
+print('\nВсе гости разошлись!')
