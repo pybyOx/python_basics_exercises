283cf8d5c7e9f39e492eefc0bcf8d8767b61525b
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:42 +0700

    task 03_random_competition decomposition completed

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,9 +1,15 @@
 import random
 
-first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+
+def create_team():
+    team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+    return team
+
+
+first_team = create_team()
 print('Первая команда:', first_team)
 
-second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_team = create_team()
 print('\nВторая команда:', second_team)
 
 winners = [max(first_team[index], second_team[index]) for index in range(20)]
