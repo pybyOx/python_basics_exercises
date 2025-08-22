ada3e39b15e96adcb56337485baecfd4425b5c7d
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:52 +0700

    commit old files

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-try:
-    ages_file = open('ages', 'r')
-except IsADirectoryError:
-    print('Указанный путь ведет к папке, а не файлу')
-
-try:
-    result_file = open('result.txt', 'x')
-except FileExistsError:
-    print('Такой файл уже существует')
+numbers = input('Введите строку: ')
+print(list(filter(lambda elem: False if elem.isdigit or elem.upper else True, numbers.split(' '))))
