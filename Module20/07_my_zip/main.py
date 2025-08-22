b42ece81df10ec1feb1520d8f7e0520ff3862e0f
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:44 +0700

    tasks 07_my_zip solved

--- Module20/07_my_zip/main.py
+++ Module20/07_my_zip/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def generator(string, tpl):
+    my_generator = ((symbol, tpl[index]) for index, symbol in enumerate(string))
+    print(my_generator)
+    for pair in my_generator:
+        print(pair)
+
+
+user_string = 'abcd'
+user_tpl = (10, 20, 30, 40)
+
+generator(user_string, user_tpl)
