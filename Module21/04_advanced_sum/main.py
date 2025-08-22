65fdb67178be3953a7127a8e2a56a80d6ed3a90e
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:45 +0700

    task 04_advanced_sum solved

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def upgrade_sum(struct, total_sum=0):
+    if isinstance(struct, tuple):
+        struct = list(struct)
+    for sub_struct in struct:
+        if isinstance(sub_struct, int):
+            total_sum += sub_struct
+        else:
+            total_sum = upgrade_sum(sub_struct, total_sum)
+    return total_sum
+
+
+# print(upgrade_sum([[1, 2, [3]], [1], 3]))
+# print(upgrade_sum((1, 2, 3, 4, 5)))
