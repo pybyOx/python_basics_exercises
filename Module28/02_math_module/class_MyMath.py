da8f9ff70bb83c4691cb3e7bf5a82c9a0602f7c8
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:51 +0700

    tasks 02_math_module solved

---
+++ Module28/02_math_module/class_MyMath.py
@@ -0,0 +1,44 @@
+from typing import Union
+
+
+class MyMath:
+    """Базовый класс, выполняющий математические вычисления, связанные с фигурами."""
+    @classmethod
+    def circle_len(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Метод, вычисляющий длину окружности.
+        Args:
+            radius(Union[int, float]): Принимает радиус окружности.
+        Returns:
+            Union[int, float]: Возвращает длину окружности.
+        """
+        return 2 * 3.14 * radius
+
+    @classmethod
+    def circle_sq(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Метод, вычисляющий площадь окружности.
+        Args:
+            radius(Union[int, float]): Принимает радиус окружности.
+        Returns:
+            Union[int, float]: Возвращает площадь окружности.
+        """
+        return 3.14 * radius ** 2
+
+    @classmethod
+    def cube_volume(cls, edge: Union[int, float]) -> Union[int, float]:
+        """Метод, вычисляющий объем куба.
+        Args:
+            edge(Union[int, float]): Принимает ребро куба.
+        Returns:
+            Union[int, float]: Возвращает объем куба.
+        """
+        return edge ** 3
+
+    @classmethod
+    def sphere_surface_sq(cls, radius: Union[int, float]) -> Union[int, float]:
+        """Метод, вычисляющий площадь поверхности сферы.
+        Args:
+            radius(Union[int, float]): Принимает радиус сферы.
+        Returns:
+            Union[int, float]: Возвращает площадь поверхности сферы.
+        """
+        return 4 * 3.14 * radius ** 2
--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+from class_MyMath import MyMath
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+print(res_1)
+print(res_2)
