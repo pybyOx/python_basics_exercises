68fe83794f40e42da8cff99b7f82ae8bb8dab5f3
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:42 +0700

    made edits in task 03_random_competition

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -6,7 +6,5 @@ print('Первая команда:', first_team)
 second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
 print('\nВторая команда:', second_team)
 
-winners = [first_team[index] if first_team[index] > second_team[index]
-           else second_team[index]
-           for index in range(20)]
+winners = [max(first_team[index], second_team[index]) for index in range(20)]
 print('\nПобедители тура:', winners)
