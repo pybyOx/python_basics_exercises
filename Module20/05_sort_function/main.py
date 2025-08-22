65853ca3ce6a6c736cd177b62ad6a27728b00d3c
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:44 +0700

    tasks 05_sort_function solved

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def tpl_sort(my_tpl):
+    for i in my_tpl:
+        if not isinstance(i, int):
+            return my_tpl
+        else:
+            return sorted(my_tpl)
+
+# альтернативное решение
+# def tpl_sort(my_tpl):
+#     if ''.join(map(str, my_tpl)).replace('-', '').isdigit() and sum(my_tpl) % 1 == 0:
+#         return sorted(my_tpl)
+#     else:
+#         return my_tpl
+
+
+# tpl = (6, 3, -1, 8, 4, 10, -5)
+
+# print(tpl_sort(tpl))
+
