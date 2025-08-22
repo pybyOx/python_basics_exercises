3e6d71ffca91a297895c0719cc24a84444a64700
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:42 +0700

    task 04_slices solved

--- Module17/04_slices/main.py
+++ Module17/04_slices/main.py
@@ -1,3 +1,15 @@
 alphabet = 'abcdefg'
 
-# TODO здесь писать код
+alphabet = [alphabet[:],
+            alphabet[::-1],
+            alphabet[::2],
+            alphabet[1::2],
+            alphabet[:1],
+            alphabet[-1],
+            alphabet[3:4],
+            alphabet[-3:len(alphabet)],
+            alphabet[3:5],
+            alphabet[4:2:-1]]
+
+for index in range(10):
+    print(index + 1, ':', alphabet[index])
