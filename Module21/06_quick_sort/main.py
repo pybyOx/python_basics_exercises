9ffd532fd1a6d7adddc84d10a097483fed6b6987
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-22 17:48:45 +0700

    task 06_quick_sort solved

--- Module21/06_quick_sort/main.py
+++ Module21/06_quick_sort/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def split_list(lst):
+    return ([elem for elem in lst if elem < lst[-1]],
+            [elem for elem in lst if elem == lst[-1]],
+            [elem for elem in lst if elem > lst[-1]])
+
+
+def qsort(my_list):
+    less, equals, more = split_list(my_list)
+    if len(less) > 1:
+        less = qsort(less)
+    if len(more) > 1:
+        more = qsort(more)
+    return less + equals + more
+
+
+# numbers = [5, 8, 9, 4, 2, 9, 1, 8]
+# print(qsort(numbers))
