a927780434cf0244900e6146372eb26ab51fc2f9
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-24 12:41:38 +0700

    add_readme_for_17

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,5 +1,3 @@
-length_list = int(input('Введите длину списка: '))
 
-result = [1 if number % 2 == 0 else number % 5 for number in range(length_list)]
-
-print(result)
+print([1 if number % 2 == 0 else number % 5
+       for number in range(int(input('Введите длину списка: ')))])
