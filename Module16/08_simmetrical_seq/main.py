ddfa86c844ba6a01b6ad000c16a68ef75b0a9571
Oksana Ushakova <oksanchik123456@mail.ru>
2025-08-24 11:38:34 +0700

    add_readme_for_16

--- Module16/08_simmetrical_seq/main.py
+++ Module16/08_simmetrical_seq/main.py
@@ -1,47 +1,24 @@
-def create_list(amt):
-    my_list = []
-    for score in range(amt):
-        print('Число №', score + 1, ':', end=' ')
-        number = int(input())
-        my_list.append(number)
-    return my_list
+
+# def is_symmetry(my_list):      если нужно найти палиндром циклом
+#     n = len(my_list)
+#     for i in range(n // 2):
+#         if my_list[i] != my_list[n - i - 1]:
+#             return False
+#     return True
+
+def is_symmetry(my_list):
+    return my_list == my_list[::-1]
 
 
-def symmetry_checking(my_list):
-    subtrahend = 1
-    amt = 0
-    for index in range(len(my_list)//2 + 1):
-        if my_list[index] == my_list[index - subtrahend]:
-            subtrahend += 2
-            amt += 1
-    if amt == len(my_list)//2 + 1:
-        return True
-    else:
-        return False
+numbers_list = [int(input(f'Число № {i + 1}: '))
+                for i in range(int(input('Кол-во чисел: ')))]
 
+print('\nПоследовательность:', numbers_list)
 
-numbers_amt = int(input('Кол-во чисел: '))
-
-numbers_list = create_list(numbers_amt)
-
-print('Последовательность:', numbers_list)
-
-symmetry = symmetry_checking(numbers_list)
-
-if symmetry:
-    print('Последовательность является симметричной')
-else:
-    asymmetrical_list = []
-
-    while True:
-        asymmetrical_list.append(numbers_list[0])
-        numbers_list.remove(numbers_list[0])
-
-        if symmetry_checking(numbers_list):
-            print('Нужно приписать чисел:', len(asymmetrical_list))
-            asymmetrical_list.reverse()
-            print('Сами числа:', asymmetrical_list)
-            break
-
-        else:
-            continue
+for i in range(len(numbers_list)):
+    part = numbers_list[i:]
+    if is_symmetry(part):
+        to_add = numbers_list[:i][::-1]
+        print('Нужно приписать чисел:', len(to_add))
+        print('Сами числа:', to_add)
+        break
